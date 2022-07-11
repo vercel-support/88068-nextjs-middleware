f471236275f424197ac7c18942764ac730ecacdb
William Li <email@williamli.dev>
2022-07-11 18:53:05 +0800

    revise redirect logic

--- middleware.ts
+++ middleware.ts
@@ -3,12 +3,22 @@ import type { NextRequest } from 'next/server';
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  const newURL = new URL('/', request.url);
-  console.log('redirecting to', { newURL });
-  return NextResponse.redirect(newURL);
+  if (
+    request.nextUrl.pathname === '/' ||
+    request.nextUrl.pathname === '/favicon.ico' ||
+    request.nextUrl.pathname.startsWith('/_next')
+  ) {
+    return NextResponse.next();
+  } else {
+    const newURL = new URL('/', request.url);
+
+    console.log('redirecting to', { newURL });
+
+    return NextResponse.redirect(newURL);
+  }
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: '/(.+)',
+  matcher: '/:path*',
 };
